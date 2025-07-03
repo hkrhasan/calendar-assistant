764f933fa9cef8ada2e4c32d88f5d98ac95c31ce
hkrhasan <hkrhasan8010@gmail.com>
2025-07-03 07:47:24 +0000

    add create-credentials.py

---
+++ create_credentials.py
@@ -0,0 +1,10 @@
+import os
+import json
+
+credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+if credentials_json:
+    with open("/app/config/credentials.json", "w") as f:
+        json.dump(json.loads(credentials_json), f)
+    print("Google credentials created from environment variable")
+else:
+    print("GOOGLE_CREDENTIALS_JSON not set")
